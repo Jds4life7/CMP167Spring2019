b80e0592da184dfe3ad50b657d222d2a4a8f31dc
Jds4life7 <47428061+Jds4life7@users.noreply.github.com>
2019-03-14 12:54:48 -0400

    Add files via upload

--- Area.java
+++ Area.java
@@ -1,14 +1,26 @@
 /*
+
  *Area.java
  * Author	: Diana Ramirez
  * Created	: 03.12.19
  * Desc		: This program implements area functions/methods
+ * Modified	: 03.14.19
  */
 
 import java.util.Scanner;
 public class Area {
 							// Scanner object
 	static Scanner input = new Scanner(System.in);
+	
+	/*
+	 * getRectArea : int
+	 * params : width: int, length: int
+	 * calculates rectangle area
+	 */
+	public static int getRectArea( int width, int length) {
+		return width * length;
+	}
+	
 	/*
 	 * rectArea : void
 	 * inputs : width and length
@@ -19,19 +31,25 @@ public class Area {
 		int width;
 		int length;
 		int area;
-		
+
 		System.out.println("Let's calculate the area ");
 		// input
 		System.out.println("Enter the width: ");
 		width = input.nextInt();
-		
 		System.out.println("Enter the length: ");
 		length = input.nextInt();
+		
 		// process/calculations
 		area = width * length;
+		
 		// output
 		System.out.println("The rect area is " + area);
 	}
+	
+	public static double getCircArea(double PI, double r) {
+		return Math.PI*r*r;
+	}
+
 // Circle Area
 	/*
 	 * circArea : void
@@ -39,23 +57,64 @@ public class Area {
 	 * process : area = pi * r * r
 	 * output : circle area
 	*/
+
 	public static void circArea () {
 		double r;
 		double pi = 3.14159265359;
 		double area;
-		
+
 		System.out.println("Let's calculate the area ");
+
 		// input
 		System.out.println("Enter the radius: ");
 		r = input.nextDouble();
 		
 		// process/calculations
 		area = pi *( r * r);
+		
 		// output
 		System.out.println("The circ area is " + area);
 	}
+
+		//for debugging
+		public static void main(String[] args) {
+			rectArea();//Function call/invocation
+			circArea();
+			triArea();
+			////
+			System.out.println("Today code");
+			System.out.println("Enter width and length: ");
+			int width = input.nextInt();
+			int length = input.nextInt();
+		
+			int area = getRectArea(width,length);
+		
+			System.out.println("Area: " + area);	
+			
+			
+			System.out.println("Enter radius: ");
+			double r = input.nextDouble();
+					
+			double carea = getCircArea(r, Math.PI);
+			
+			System.out.println("Area: " + carea );		
+			
+			
+			System.out.println("Enter base and height: ");
+			
+			double b = input.nextInt();
+			double h = input.nextInt();
+		
+			double tarea = getTriArea(b, h);
+		
+			System.out.println("Area: " + tarea);		
+	}		
+		
+		public static double getTriArea( double base, double height) {
+			return (base * height)/2;
+		}
 	
-// Triangle Area
+		// Triangle Area
 		/*
 		 * triArea : void
 		 * inputs : height, base
@@ -74,19 +133,13 @@ public class Area {
 			System.out.println("Enter the base: ");
 			base = input.nextDouble();
 			
+
 			// process/calculations
 			area = (height * base) / 2 ;
 			// output
-			System.out.println("The triangle area is " + area);
+			System.out.println("The triangle area is " + area);	
+				
 		}
 		
-		// for debugging
-		public static void main(String[] args) {
-			rectArea();//Function call/invocation
-			circArea();
-			triArea();	
-		}
-			
-		}
-	
 
+}
