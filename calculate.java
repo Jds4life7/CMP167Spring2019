ddc93f691a03d80f61f6c66b7b240c7ba8a4129b
Jds4life7 <47428061+Jds4life7@users.noreply.github.com>
2019-04-02 12:20:52 -0400

    Add files via upload

---
+++ calculate.java
@@ -0,0 +1,18 @@
+/*
+ * Author:	Diana Ramirez
+ * Created:	4.2.19
+ * Desc:	This class has calculate methods	
+ */
+
+public class calculate {
+
+	public static int multiply(int a, int b) {
+		return a * b;
+	}
+	public static double multiply(double a, double b) {
+		return a * b;
+	}
+	public static double getRectangleArea(double width, double length) {
+		return width * length;
+	}
+}
