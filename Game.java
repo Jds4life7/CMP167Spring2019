773b6e34273ed83de52327219222ea3bae0741e4
Jds4life7 <47428061+Jds4life7@users.noreply.github.com>
2019-04-16 12:33:16 -0400

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,46 @@
+
+public class Game {
+	public static void main(String[]args ) {
+		human person1 = new human(); // Creating human object
+		human person2= new human();
+		person2.age=5;
+		System.out.println("age" +person2.age);
+		System.out.println("age" +person1.age);
+		
+	/*	
+		System.out.println("Hunger: " +person1.hunger);
+		System.out.println("Food: " +person1.food);
+		
+		System.out.println("Eating command: ");
+		person1.eat();
+		System.out.println("Eating command: ");
+		person1.eat();
+		System.out.println("Eating command: ");
+		person1.eat();
+		
+		System.out.println("Hunger: " +person1.hunger);
+		System.out.println("Food: " +person1.food);
+		
+		System.out.println("Buying food command: ");
+		person1.buyFood();
+		
+		System.out.println("Working command: ");
+		person1.work();
+		
+		System.out.println("Buying food command: ");
+		person1.buyFood();
+		
+		System.out.println("Working command: ");
+		person1.work();
+		System.out.println("Working command: ");
+		person1.work();
+		
+		System.out.println("Eating command: ");
+		person1.eat();
+		
+		System.out.println("Working command: ");
+		person1.work();
+		*/
+		
+	}
+}
--- human.java
+++ human.java
@@ -4,36 +4,67 @@
  * 
  */
 public class human {
-	public int age;
+	public int age;		// They belong to the object, they are calling
 	public int food;
 	public double wealth;
 	public double hunger;
 	public String name;
-	public boolean hasVehicle;	
+	public boolean hasVehicle;
+	public vehicle[] vehicles;
+
+	public human() { //Constructor new Human();
+		this.age		= 0;	//this refers to the object
+		this.name		= "";
+		this.wealth		= 0.0;
+		this.hasVehicle	= false;
+		this.hunger		= 5.0;
+		this.food		= 5;
+		this.vehicles	= new vehicle[100]; 	//array of vehicles
+	}
+	public int numberOfVehicles() {
+		int total = 0;
+		for(int i=0; i<vehicles.length; i++) {
+			if(vehicles[i] != null)
+				total++;
+			else
+				break;
+		}
+		return total;
+	}
 	
 	public void eat() {
-		if(food>0) 
-			hunger--;
+		if(food>4.5) {
+			hunger 	-= 1.0;
+			food 	-= 1;
+			System.out.println("Yummy, I ate all");
+		}
 		else 
 			System.out.println("Not enough food, please buy food");
 	}
+
 	public void grow( ) {
 		age++;
 	}
 	public void work( ) {
-		if(hunger<5)
+		if(hunger<5) {
 			wealth += 10.5;
+			hunger += 0.5;
+			System.out.println("Woah, I worked a lot");
+		}
 		else
 			System.out.println("Too hungry to work");
 	}
+
 	public void buyFood( ) {
 		if(wealth>5) {
 			wealth -= 4.5;
 			food ++;
+			System.out.println("Yoohoo, I bought some food");
 		}
 		else {
 			System.out.println("Not enough money, please work");
 		}
 	}
 }
+
 	
